ebc7225af433af33bb2db99e0cc7cef0fa909fc1
supreetoh <supreetoh@gmail.com>
2022-05-28 15:24:03 +0530

    initial commit

---
+++ data/blogs.js
@@ -0,0 +1,25 @@
+blogs = [
+    {
+        title: "Bitcoin : The Game Changer!",
+        content: "Bitcoin is a decentralized digital currency that can be transferred on the peer-to-peer bitcoin network. Bitcoin transactions are verified by network nodes through cryptography and recorded in a public distributed ledger called a blockchain. ",
+        slug: "btc-learn"
+    },
+    {
+        title: "Ethereum : The Boss Around",
+        content: "Ethereum is a decentralized, open-source blockchain with smart contract functionality. Ether is the native cryptocurrency of the platform. Among cryptocurrencies, Ether is second only to Bitcoin in market capitalization. Ethereum was conceived in 2013 by programmer Vitalik Buterin.",
+        slug: "eth-learn"
+    },
+    {
+        title: "Cardano : Older Than Age",
+        content: "Cardano is a public blockchain platform. It is open-source and decentralized, with consensus achieved using proof of stake. It can facilitate peer-to-peer transactions with its internal cryptocurrency, ADA. Cardano was founded in 2015 by Ethereum co-founder Charles Hoskinson.",
+        slug: "ada-learn"
+    },
+    {
+        title: "Dogecoin",
+        content: "Dogecoin is a cryptocurrency created by software engineers Billy Markus and Jackson Palmer, who decided to create a payment system as a joke, making fun of the wild speculation in cryptocurrencies at the time. It is considered both the first meme coin, and, more specifically, the first DOG COIN",
+        slug: "doge-learn"
+    },
+]
+
+
+module.exports = blogs;
